b665a56a96bb2adb6ac21d7ae1158e8d49ed32dc
heisenberg1316 <rawatmukul5000@gmail.com>
2025-08-14 02:15:11 +0530

    added env

--- Week-13-Project/frontend/src/api/axios.ts
+++ Week-13-Project/frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend.rawatmukul5000.workers.dev", // ✅ your backend URL here :-> http://localhost:8787 (DEV)
+  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ your backend URL here :-> http://localhost:8787 (DEV)
   withCredentials: true,                // ✅ send cookies (refresh token)
 });
 
